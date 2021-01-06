65a66017e3cf94a110f1833f11c39d920ed89fc5
William Barriga <malliwttt@gmail.com>
2021-01-06 16:24:45 -0500

    add posts aplication and templates for that

--- platzigram/urls.py
+++ platzigram/urls.py
@@ -1,8 +1,10 @@
-from platzigram import views
+from platzigram import views as local_views
 from django.urls import path
+from posts import views as posts_views
 
 urlpatterns = [
-    path('hello-world/', views.date_time),
-    path('numbers/', views.numbers),
-    path('validation/<str:name>/<int:age>/', views.say_hi)
+    path('hello-world/', local_views.date_time),
+    path('numbers/', local_views.numbers),
+    path('validation/<str:name>/<int:age>/', local_views.say_hi),
+    path('posts/', posts_views.list_posts)
 ]
---
+++ posts/views.py
@@ -0,0 +1,40 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
+
+posts = [
+    {
+        'title': 'Mont Blanc',
+        'user': {
+            'name': 'Yésica Cortés',
+            'picture': 'https://picsum.photos/60/60/?image=1027'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://imgs-tarkark.s3.us-east-2.amazonaws.com/flags.jpg',
+    },
+    {
+        'title': 'Via Láctea',
+        'user': {
+            'name': 'Christian Van der Henst',
+            'picture': 'https://picsum.photos/60/60/?image=1005'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://imgs-tarkark.s3.us-east-2.amazonaws.com/wolf.jpg',
+    },
+    {
+        'title': 'Nuevo auditorio',
+        'user': {
+            'name': 'Uriel (thespianartist)',
+            'picture': 'https://picsum.photos/60/60/?image=883'
+        },
+        'timestamp': datetime.now().strftime('%b %dth, %Y - %H:%M hrs'),
+        'photo': 'https://imgs-tarkark.s3.us-east-2.amazonaws.com/targaryen.jpg',
+    }
+]
+
+
+# Create your views here.
+def list_posts(request):
+
+    return render(request, 'feed.html', {'posts': posts})
