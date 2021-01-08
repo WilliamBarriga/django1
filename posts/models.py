fab9034bcf0b9941ff9480985229f3613593bb11
William Barriga <malliwttt@gmail.com>
2021-01-08 08:49:17 -0500

    models.py

--- posts/models.py
+++ posts/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class User(models.Model):
+    email = models.EmailField(unique=True)
+    password = models.CharField(max_length=15)
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
+    bio = models.TextField(blank=True)
+    birthdate = models.DateField(blank=False)
+    created = models.DateTimeField(auto_now_add=True)
+    modified = models.DateTimeField(auto_now=True)
